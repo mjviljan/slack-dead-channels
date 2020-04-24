298030c63e1e1ac65412992cd8f670a69c9e91ef
Mika Viljanen <mika.viljanen@wunderdog.fi>
2020-04-24 12:44:09 +0300

    Add a report of channels ordered by inactivity

--- src/report/index.ts
+++ src/report/index.ts
@@ -3,18 +3,50 @@ import { WebClient } from '@slack/web-api'
 import getChannels, { Channel } from '../channels'
 import getChannelMessages, { Message } from '../messages'
 
-type ChannelWithActivityTimestamp = Channel & {
-    latestTimestamp?: number
-}
+const DAY_IN_SECS = 60 * 60 * 24
 
 const hasLessThan3Members = (channel: Channel): boolean => channel.num_members < 3
 
+const byLastActivity = (channel1: ChannelWithActivityTimestamp, channel2: ChannelWithActivityTimestamp) => {
+    if (!channel1.latestTimestamp) return -1 // channel 1 has never had messages
+
+    if (!channel2.latestTimestamp) return 1 // channel 2 has never had messages
+
+    return channel1.latestTimestamp - channel2.latestTimestamp
+}
+
 const byMemberCount = (channel1: Channel, channel2: Channel) => channel1.num_members - channel2.num_members
 
-const toReportRow = (acc: string, channel: Channel): string => acc + `${channel.name} (${channel.num_members})\n`
+const timestampToString = (timestamp: number | undefined): string => {
+    if (!timestamp) return 'never'
+
+    const now = new Date().getTime() / 1000
+    const daysSinceActive = Math.floor((now - timestamp) / DAY_IN_SECS)
+
+    return daysSinceActive ? daysSinceActive + ' days' : 'today'
+}
+
+const toActivityReportRow = (acc: string, chan: ChannelWithActivityTimestamp): string =>
+    acc + `${chan.name} (${timestampToString(chan.latestTimestamp)})\n`
+
+const toMemberCountReportRow = (acc: string, chan: Channel): string => acc + `${chan.name} (${chan.num_members})\n`
+
+export type ChannelWithActivityTimestamp = {
+    name: string
+    latestTimestamp?: number
+}
+
+export const listChannelsByInactiveDays = (channels: ChannelWithActivityTimestamp[]): string => {
+    const channelReport = channels.sort(byLastActivity).reduce(toActivityReportRow, '')
+
+    return 'Channels ordered by inactivity\n' + '------------------------------\n' + channelReport
+}
 
 export const listSmallChannels = (channels: Channel[]): string => {
-    const channelReport: string = channels.filter(hasLessThan3Members).sort(byMemberCount).reduce(toReportRow, '')
+    const channelReport: string = channels
+        .filter(hasLessThan3Members)
+        .sort(byMemberCount)
+        .reduce(toMemberCountReportRow, '')
 
     return channelReport
         ? 'Channels with less than 3 members:\n----------------------------------\n' + channelReport
@@ -24,23 +56,20 @@ export const listSmallChannels = (channels: Channel[]): string => {
 export default async function reportChannelsByActivity(apiClient: WebClient): Promise<string> {
     const channels = await getChannels(apiClient)
 
-    const emptyToDisableMessageFetching: Channel[] = []
     const channelsWithTimestamps = await Promise.all(
-        emptyToDisableMessageFetching.map(
+        channels.map(
             async (c: Channel): Promise<ChannelWithActivityTimestamp> => {
                 const latestMessage: Message | undefined = await getChannelMessages(apiClient, c.id)
-                console.log('MEssage:', latestMessage)
 
                 if (!latestMessage) {
                     return c
                 }
 
-                const latestTimestamp: number = parseInt(latestMessage.ts.split('.')[0])
+                const latestTimestamp: number = parseInt(latestMessage.ts)
                 return { ...c, latestTimestamp }
             },
         ),
     )
 
-    !!channelsWithTimestamps
-    return listSmallChannels(channels)
+    return listChannelsByInactiveDays(channelsWithTimestamps) + '\n' + listSmallChannels(channels)
 }
--- test/report/report.spec.ts
+++ test/report/report.spec.ts
@@ -1,7 +1,42 @@
 import * as fakeChannels from './fakeChannels.json'
-import { listSmallChannels } from '../../src/report'
+import { ChannelWithActivityTimestamp, listChannelsByInactiveDays, listSmallChannels } from '../../src/report'
+
+const DAY_IN_SECS = 60 * 60 * 24
 
 describe('Reporting channels by activity', () => {
+    test('lists channels by inactive days in decremental order', () => {
+        const now = new Date().getTime() / 1000
+        const channelsWithTimestamps: ChannelWithActivityTimestamp[] = [
+            {
+                name: 'channel_uno',
+                latestTimestamp: now,
+            },
+            {
+                name: 'channel_dos',
+                latestTimestamp: now - 25 * DAY_IN_SECS - 123, // some offset to test Math.floor
+            },
+            {
+                name: 'channel_tres',
+                latestTimestamp: now - 450 * DAY_IN_SECS,
+            },
+            {
+                name: 'channel_cuatro',
+                latestTimestamp: undefined,
+            },
+        ]
+
+        const expectedReport =
+            'Channels ordered by inactivity\n' +
+            '------------------------------\n' +
+            'channel_cuatro (never)\n' +
+            'channel_tres (450 days)\n' +
+            'channel_dos (25 days)\n' +
+            'channel_uno (today)\n'
+        const report = listChannelsByInactiveDays(channelsWithTimestamps)
+
+        expect(report).toEqual(expectedReport)
+    })
+
     test('lists small (with less than 3 members) channels by member count in incremental order', () => {
         const expectedReport =
             'Channels with less than 3 members:\n' +
